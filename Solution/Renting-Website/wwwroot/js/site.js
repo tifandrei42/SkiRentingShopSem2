becdeed3469b7bc7335ba740c421b753deaa16b1
Andrei Ţifrea <I455422@fhict.nl>
2024-10-04 17:03:39 +0200

    Uploaded project plan, ideation document and changed website layout

--- Solution/Renting-Website/wwwroot/js/site.js
+++ Solution/Renting-Website/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    document.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+    const parallaxContainer = document.querySelector('.parallax-container');
+
+    // Create a reversed parallax effect where the background image moves
+    const offset = -scrollPosition * 0.3; // Adjust multiplier to change speed and direction
+    parallaxContainer.style.backgroundPositionY = `${offset}px`;
+    });
+</script>
+
+
+
